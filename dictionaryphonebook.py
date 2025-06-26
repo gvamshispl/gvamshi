bdddece4cc968cdc87a9a3fd58e1c58f260c2e6b
gvamshispl <gvamshispl@gmail.com>
2025-06-26 16:31:39 +0530

    Create dictionaryphonebook.py
    to get your phone number give your name

---
+++ dictionaryphonebook.py
@@ -0,0 +1,7 @@
+dictionary={"vamshi":9505637615,"abhi":9963827486,"teja":8309188335,"ravi":8642369721,"adhi":7894561596}
+name=input("enter your name:")
+
+if name in dictionary.keys():
+    print(dictionary[name])
+else:
+    print("")
