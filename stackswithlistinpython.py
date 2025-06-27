63d1f4ab9c4fedc00ba8e0f10d5eed0b7b224207
gvamshispl <gvamshispl@gmail.com>
2025-06-27 10:44:37 +0530

    Create stackswithlistinpython.py
    stacks with list in python

---
+++ stackswithlistinpython.py
@@ -0,0 +1,27 @@
+stack=[]
+for i in  range(5):
+    ele=int(input("enter a number :"))
+    stack.append(ele)
+print(stack)
+
+def push(element):
+    stack.append(element)
+    print("stack after adding element:",stack)
+
+def pop():
+    e=stack.pop()
+    print("poped element is:",e)
+    print("stack after poping:",stack)
+
+def peek():
+    top=stack[len(stack)-1]
+    print("top element is:",top)
+    
+push(100)
+push(200)
+pop()
+peek()
+
+
+
+                                                                                                                                                                                                                                                                                                                                           
